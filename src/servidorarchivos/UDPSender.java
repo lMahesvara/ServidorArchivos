736973f8245db22c6766f72f5692bed78bd31f4b
Erick Bernal Amparano <erick082016@gmail.com>
2023-02-26 22:05:54 -0700

    first commit

---
+++ src/servidorarchivos/UDPSender.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package servidorarchivos;
+
+/**
+ *
+ * @author tonyd
+ */
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class UDPSender {
+
+    private static final int PACKET_SIZE = 60000;
+    private static final int TIMEOUT_MS = 5000;
+    private static final int MAX_TRIES = 3;
+    private static int EXTRA_BYTES = 4;
+
+    public static void main(String[] args) throws Exception {
+        String hostName = "localhost";
+        int port = 12345;
+
+        DatagramSocket socket = new DatagramSocket();
+        socket.setSoTimeout(0);
+        InetAddress address = InetAddress.getByName(hostName);
+
+        String filePath = "D:\\Documents\\videos\\entrevista_229254.mp3";
+        byte[] messageBytes  = Files.readAllBytes(Paths.get(filePath));
+        System.out.println(messageBytes.length);
+
+        int totalPackets = (int) Math.ceil((double) messageBytes.length / PACKET_SIZE);
+
+        byte[] buffer = new byte[PACKET_SIZE + EXTRA_BYTES];
+        
+        byte[] totalPacketsBytes = getBytesFromValue(totalPackets);
+        
+        buffer[0] = totalPacketsBytes[0];
+        buffer[1] = totalPacketsBytes[1];
+        
+        DatagramPacket packet = new DatagramPacket(buffer, 2, InetAddress.getByName(hostName), port);
+        socket.send(packet);
+
+        int packetNumber = 0;
+        int offset = 0;
+
+        while (offset < messageBytes.length) {
+
+            packetNumber++;
+            int length = Math.min(PACKET_SIZE, messageBytes.length - offset);
+            byte[] packetSize = getBytesFromValue(packetNumber);
+            
+            byte[] lengthBytes = getBytesFromValue(length);
+            
+            buffer[0] = packetSize[0];
+            buffer[1] = packetSize[1];
+            buffer[2] = lengthBytes[0];
+            buffer[3] = lengthBytes[1];
+
+            while (true) {
+
+                //Enviar paquete
+                System.arraycopy(messageBytes, offset, buffer, 4, length);
+                packet = new DatagramPacket(buffer, (length + 4), InetAddress.getByName(hostName), port);
+                socket.send(packet);
+
+                // Esperar la confirmación del receptor
+                byte[] bufferEsperado = new byte[PACKET_SIZE];
+                DatagramPacket confirmationPacket = new DatagramPacket(bufferEsperado, bufferEsperado.length);
+                socket.receive(confirmationPacket);
+
+                // Verificar si se recibió la confirmación correcta
+                if (isConfirmationPacketCorrect(confirmationPacket.getData(), packetNumber)) {
+                    System.out.println("Packet " + packetNumber + " sent successfully");
+                    break;
+                } else {
+                    System.out.println("Packet " + packetNumber + " not received correctly, retrying...");
+                }
+
+                // Si se han intentado MAX_TRIES veces y no se ha recibido confirmación, asumir que se ha perdido el paquete
+                if (packetNumber % MAX_TRIES == 0) {
+                    try {
+                        socket.setSoTimeout(TIMEOUT_MS);
+                        socket.receive(confirmationPacket);
+                    } catch (SocketTimeoutException e) {
+                        System.out.println("Packet " + packetNumber + " lost, retrying...");
+                    }
+                }
+
+            }
+            offset += length;
+        }
+
+        System.out.println("Se han enviado " + totalPackets + " paquetes.");
+
+        // Esperar la respuesta del servidor
+        socket.setSoTimeout(0);
+        byte[] ackBuffer = new byte[PACKET_SIZE];
+        DatagramPacket ackPacket = new DatagramPacket(ackBuffer, ackBuffer.length);
+        try {
+            socket.receive(ackPacket);
+            String ackMessage = new String(ackPacket.getData(), 0, ackPacket.getLength());
+            System.out.println("Confirmación recibida: " + ackMessage);
+        } catch (SocketTimeoutException e) {
+            System.out.println("No se recibió confirmación del servidor.");
+        }
+
+        socket.close();
+    }
+
+    private static boolean isConfirmationPacketCorrect(byte[] data, int expectedPacketNumber) {
+        int packetNumber = getValueFromBytes(data[0],data[1]);
+        //System.out.printf("Numero: %d ---- esperado: %d%n",packetNumber,expectedPacketNumber);
+        return packetNumber == expectedPacketNumber;
+    }
+    
+    private static byte[] getBytesFromValue(int value){
+        byte[] bytes = new byte[2]; // array de bytes para almacenar los resultados
+            // dividir valor en dos bytes
+            bytes[0] = (byte) ((value >> 8) & 0xFF); // byte más significativo
+            bytes[1] = (byte) (value & 0xFF); // byte menos significativo
+            return bytes;
+    }
+
+    private static int getValueFromBytes(byte byte1, byte byte2){
+        return ((byte1 & 0xFF) << 8) | (byte2 & 0xFF);
+    }
+
+}
